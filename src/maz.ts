4422f0de606e5ec66f557edf7d4da9b802128d03
James Ots <code@jamesots.com>
2017-07-14 19:24:42 +0100

    Show errors nicely

--- src/maz.ts
+++ src/maz.ts
@@ -1,25 +1,51 @@
 import * as compiler from './compiler';
 import * as commandLineArgs from 'command-line-args';
+import * as commandLineUsage from 'command-line-usage';
 import * as fs from 'fs';
 
 const optionDefinitions = [
     { name: 'src', alias: 's', type: String, multiple: false, defaultOption: true },
     { name: 'out', alias: 'o', type: String, multiple: false },
-    { name: 'help', alias: 'h' }
+    { name: 'help', alias: 'h', type: Boolean, multiple: false }
 ];
 const options = commandLineArgs(optionDefinitions);
 
-console.log("MAZ v0.1.0");
+function showUsage() {
+    console.log(commandLineUsage([
+        {
+            header: 'MAZ v0.1.0',
+            content: 'Macro Assembler for Z80'
+        },
+        {
+            header: 'Options',
+            optionList: optionDefinitions
+        }]));
+}
 
-console.log(JSON.stringify(options));
+if (!options.src || !options.out || options.help) {
+    showUsage();
+    process.exit(-1);
+}
+
+console.log("MAZ v0.1.0");
 
 const source = fs.readFileSync(options.src).toString();
 
-let [ast, symbols] = compiler.compile(source);
-console.log(JSON.stringify(ast, undefined, 2));
-console.log(JSON.stringify(symbols, undefined, 2));
-const bytes = compiler.getBytes(ast);
-console.log(JSON.stringify(bytes, undefined, 2));
+try {
+    let [ast, symbols] = compiler.compile(source);
+    console.log(JSON.stringify(ast, undefined, 2));
+    console.log(JSON.stringify(symbols, undefined, 2));
+    const bytes = compiler.getBytes(ast);
+    console.log(JSON.stringify(bytes, undefined, 2));
 
-fs.writeFileSync(options.out, Buffer.from(bytes));
+    fs.writeFileSync(options.out, Buffer.from(bytes));
 
+} catch (e) {
+    if (e.name === "SyntaxError") {
+        console.log(`Syntax error on line ${e.location.start.line} col ${e.location.start.column}`);
+        console.log('> ' + source.split('\n')[e.location.start.line - 1]);
+        console.log('> ' + ' '.repeat(e.location.start.column - 1) + '^');
+    } else {
+        console.log(e);
+    }
+}
