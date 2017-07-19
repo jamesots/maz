43e4bf73a2466dbb5fe5de0526fc4d7f81eeff72
James Ots <code@jamesots.com>
2017-07-19 21:39:31 +0100

    Make maz runnable, maybe

---
+++ README.md
@@ -0,0 +1,8 @@
+maz
+===
+
+Macro Assembler for Z80
+-----------------------
+
+Maz is a Z80 macro assembler, which is currently under development. I wouldn't advise using it until it's at version 1, or at least until I change this message.
+
--- src/maz.ts
+++ src/maz.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import * as compiler from './compiler';
 import * as commandLineArgs from 'command-line-args';
 import * as commandLineUsage from 'command-line-usage';
@@ -32,19 +33,32 @@ if (!options.src || !options.out || options.help) {
 }
 
 console.log("MAZ v0.1.0");
+console.log("WARNING: maz is under development, and likely to break without");
+console.log("         warning, and future versions will probably be completely");
+console.log("         incompatible.");
 
 const source = fs.readFileSync(options.src).toString();
+console.log(`Compiling ${options.src}`);
 
 try {
     let [ast, symbols] = compiler.compile(source, {trace: false});
-    console.log(JSON.stringify(ast, undefined, 2));
-    console.log(JSON.stringify(symbols, undefined, 2));
+    // console.log(JSON.stringify(ast, undefined, 2));
+    // console.log(JSON.stringify(symbols, undefined, 2));
     const bytes = compiler.getBytes(ast);
     // console.log(JSON.stringify(bytes, undefined, 2));
-    const list = compiler.getList(source, ast, symbols);
-    console.log(JSON.stringify(list, undefined, 2)); 
 
     fs.writeFileSync(options.out, Buffer.from(bytes));
+    console.log(`Written ${bytes.length} ($${bytes.length.toString(16)}) bytes ${options.out}`);
+    if (options.list) {
+        const list = compiler.getList(source, ast, symbols);
+        const file = fs.openSync(options.list, 'w');
+        for (const line of list) {
+            fs.writeSync(file, line);
+            fs.writeSync(file, '\n');
+        }
+        fs.closeSync(file);
+    }
+    console.log(`List written to ${options.list}`);
 
 } catch (e) {
     if (e.name === "SyntaxError") {
